7cc9f50f3b08b070c0c6aba3fdcff4d1f19ef779
Nuno Farinha <nadfarinha@gmail.com>
2019-03-26 17:51:41 +0000

    Create get author function on Book class

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,25 @@
 public class Book {
+
+    private String title;
+    private String author;
+    private String genre;
+
+    public Book() {
+    }
+
+    public void addTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+    public void addAuthor(String newAuthor) {
+        this.author = newAuthor;
+    }
+
+    public void addGenre(String newGenre) {
+        this.genre = newGenre;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
 }
--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+   Book book1;
+   Book book2;
+   Book book3;
+
+   @Before
+    public void before() {
+       book1 = new Book();
+       book1.addAuthor("J. R. R. Tolkien");
+       book1.addTitle("The Lord of The Rings");
+       book1.addGenre("Fantasy");
+
+       book2 = new Book();
+       book2.addAuthor("Frank Herbert");
+       book2.addTitle("Dune");
+       book2.addGenre("Science Fiction");
+
+       book3 = new Book();
+       book3.addAuthor("George Orwell");
+       book3.addTitle("Nineteen Eighty-Four");
+       book3.addGenre("Dystopian");
+   }
+
+   @Test
+    public void hasAuthor() {
+      book1.addAuthor("Tolkien");
+       assertEquals("Tolkien", book1.getAuthor());
+   }
+
+
 }
