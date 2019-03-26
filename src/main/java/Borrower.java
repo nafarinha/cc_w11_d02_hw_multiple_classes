6bf6bfc660a786b0fb49fb2a7f6628a0b7678ca2
Nuno Farinha <nadfarinha@gmail.com>
2019-03-26 21:29:49 +0000

    Create add borrowed book function to Borrower class

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Borrower {
+    private ArrayList<Book> borrowedBooks;
+
+    public Borrower() {
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public int bookCount() {
+        return this.borrowedBooks.size();
+    }
+
+    public void addBorrowedBook(Book newBook) {
+        this.borrowedBooks.add(newBook);
+
+    }
 }
--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,2 +1,57 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
+
+    Library library;
+    Book book1, book2, book3;
+    Borrower borrower;
+
+
+    @Before
+    public void before() {
+        library = new Library();
+
+        book1 = new Book();
+        book1.addAuthor("J. R. R. Tolkien");
+        book1.addTitle("The Lord of The Rings");
+        book1.addGenre("Fantasy");
+
+        book2 = new Book();
+        book2.addAuthor("Frank Herbert");
+        book2.addTitle("Dune");
+        book2.addGenre("Science Fiction");
+
+        book3 = new Book();
+        book3.addAuthor("George Orwell");
+        book3.addTitle("Nineteen Eighty-Four");
+        book3.addGenre("Dystopian");
+
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+
+        borrower = new Borrower();
+    }
+
+    @Test
+    public void getBookCount() {
+        assertEquals(0, borrower.bookCount());
+    }
+
+    @Test
+    public void addBorrowedBook() {
+        borrower.addBorrowedBook(book1);
+        assertEquals(1, borrower.bookCount());
+    }
+
+//    @Test
+//    public void borrowBook() {
+//        borrower.borrow(book1);
+//
+//        assertEquals(2, library.bookCount());
+//        assertEquals(1, borrower.bookCount());
+//    }
 }
